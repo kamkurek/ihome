aa58b58bfdeb81609dd718288d67ae8e13970e74
Kamil Kurek <kamkurek@gmail.com>
2016-09-12 18:44:50 +0200

    Created database structure sql

---
+++ sql/database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE data
+(
+  sensor_id TEXT NOT NULL,
+  date TEXT NOT NULL,
+  value TEXT NOT NULL
+);
+
+CREATE TABLE sensor_name
+(
+  id TEXT NOT NULL,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE sensors
+(
+  id TEXT NOT NULL,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE widget_params
+(
+  widget_id TEXT NOT NULL,
+  parameter TEXT NOT NULL,
+  value TEXT NOT NULL
+);
